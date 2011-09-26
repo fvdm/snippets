126b2b8d1f7543cbb292b4928adc0ca457a582b8
Franklin <fr@nkl.in>
2011-09-26 22:53:50 +0200

    Incomplete function name

--- array_missing.php
+++ array_missing.php
@@ -1,6 +1,6 @@
 <?php
 # Find out which keys are missing in an array
-function missing( &$arr, $mustHave )
+function array_missing( &$arr, $mustHave )
 {
 	$mustHave = explode(',', $mustHave);
 	$missing = array_diff_assoc( $mustHave, array_keys($arr) );
