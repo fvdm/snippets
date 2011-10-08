ccc26d849f41e136e85f6b522d0b71be547b1755
Franklin <fr@nkl.in>
2011-10-08 16:43:07 +0200

    iterator_to_array() is a PHP >5.1.0 feature

--- object2array.php
+++ object2array.php
@@ -2,7 +2,7 @@
 # object to array
 public function object2array($o)
 {
-	$o = $o instanceof Traversable ? iterator_to_array($o) : $o; # iterator
+	$o = $o instanceof Traversable && version_compare( PHP_VERSION, '5.1.0', '>=' ) ? iterator_to_array($o) : $o; # iterator
 	$o = is_object($o) ? get_object_vars($o) : $o; # object
 	if( is_array($o) ) # array
 	{
