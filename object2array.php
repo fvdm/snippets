5611616de7177e54bcb8c11950ccb5c4264f44e1
Franklin <fr@nkl.in>
2011-10-08 16:42:21 +0200

    Compressed for simple copy/paste

--- object2array.php
+++ object2array.php
@@ -2,29 +2,15 @@
 # object to array
 public function object2array($o)
 {
-	# $o is iterator
-	if( $o instanceof Traversable )
+	$o = $o instanceof Traversable ? iterator_to_array($o) : $o; # iterator
+	$o = is_object($o) ? get_object_vars($o) : $o; # object
+	if( is_array($o) ) # array
 	{
-		$o = iterator_to_array($o);
-	}
-	
-	# $o is object
-	if( is_object($o) )
-	{
-		$o = get_object_vars($o);
-	}
-	
-	# $o is array
-	if( is_array($o) )
-	{
-		# recurse elements
 		foreach( $o as $k => $v )
 		{
 			$o[ $k ] = object2array($v);
 		}
 	}
-	
-	# $o is normal
 	return $o;
 }
 ?>
