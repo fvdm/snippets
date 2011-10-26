9ebec77197194dc839e4f419bfa2b5d5dcacb108
Franklin <fr@nkl.in>
2011-10-26 23:50:44 +0200

    Compacted a little bit for easier copy+paste

--- pre.php
+++ pre.php
@@ -1,23 +1,17 @@
 <?php
 // Return any var as string wrapped in <pre> tags
-function pre( $var, $tags=true, $doPrint=false )
-{
-	$str = $tags ? '<pre>' : '';
+function pre( $var, $tags=true, $doPrint=false ) {
 	ob_start();
-	if( is_object($var) || is_array($var) )
-	{
+	if( is_object($var) || is_array($var) ) {
 		print_r($var);
-	}
-	else
-	{
+	} else {
 		echo $var;
 	}
-	$str .= ob_get_contents();
+	$str = ob_get_contents();
 	ob_end_clean();
-	$str .= $tags ? '<pre>' : '';
+	$str = $tags ? '<pre>'. $str .'</pre>' : $str;
 	
-	if( $doPrint )
-	{
+	if( $doPrint ) {
 		echo $str;
 	}
 	
